7503bed0b05af9588bb610c10191006952395406
Ubuntu <hackfmi_user@eu-west-hackfmi-server.bggbp1qplqxu5j4pjeotrhfjmf.ax.internal.cloudapp.net>
2016-12-16 15:44:04 +0000

    randomize port

--- python/scripts/start_notebook.sh
+++ python/scripts/start_notebook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd notebook
-PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip 0.0.0.0" /opt/PredictionIO-0.10.0-incubating/vendors/spark-1.5.1-bin-hadoop2.6/bin/pyspark
+PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip 0.0.0.0 --port `shuf -i8000-9000 -n1`" /opt/PredictionIO-0.10.0-incubating/vendors/spark-1.5.1-bin-hadoop2.6/bin/pyspark
