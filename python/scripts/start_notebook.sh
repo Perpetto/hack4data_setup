3adc096811ade781b4f2903e85ee51e87f3ba99c
Yavor Stoychev <stoychev.yavor@gmail.com>
2016-12-16 16:29:28 +0200

    change notebook startup dir and script

--- python/scripts/start_notebook.sh
+++ python/scripts/start_notebook.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd notebook
 PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS="notebook --ip 0.0.0.0" PredictionIO-0.10.0-incubating/vendors/spark-1.5.1-bin-hadoop2.6/bin/pyspark
--- ruby/Readme.md
+++ ruby/Readme.md
@@ -1,5 +1,6 @@
 # Install Dependencies
 ```
+cd ruby
 bundle install
 ```
 
